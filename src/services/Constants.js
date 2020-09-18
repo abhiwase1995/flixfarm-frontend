2b1157517844dd6a36991cab33033e739cb1caf6
abhiwase1995 <63622497+abhiwase1995@users.noreply.github.com>
2020-09-18 14:15:15 -0400

    Update Constants.js

--- src/services/Constants.js
+++ src/services/Constants.js
@@ -3,7 +3,7 @@ export const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
 export const TMDB_BACKDROP_BASE_URL = 'https://image.tmdb.org/t/p/original';
 export const API_KEY = 'c5caca19c541b3bea74c471a0a6adf98';
 
-export const HEROKU_BASE_URL = 'https://movie-user-server.herokuapp.com';
+export const HEROKU_BASE_URL = 'https://flixfarm-backend.herokuapp.com/';
 
 export const POPULAR_MOVIES_URL = TMDB_BASE_URL + '/discover/movie?api_key=' + API_KEY + '&&sort_by=popularity.desc';
 export const NOW_PLAYING_MOVIES_URL = TMDB_BASE_URL + '/movie/now_playing?api_key=' + API_KEY;
